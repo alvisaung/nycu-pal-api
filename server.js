307a8780244038116dc6dc3ad5e45ecd827c3b39
Alvis Aung <alvisaung@gmail.com>
2024-09-08 11:26:28 +0800

    Fix file size limit

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const multer = require("multer");
 const routes = require("./src/routes");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const port = 5001;
 
 // Middleware
 app.use(cors());
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api", routes);
 
