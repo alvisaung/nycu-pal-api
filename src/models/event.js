629aff66e46b9046559744a5fa1a3ccca29469c6
Alvis Aung <alvisaung@gmail.com>
2024-11-01 21:16:20 +0800

    remove set

--- src/models/event.js
+++ src/models/event.js
@@ -15,10 +15,6 @@ module.exports = (sequelize, DataTypes) => {
           // Parse JSON string if MariaDB stores it as a string
           return typeof rawValue === "string" ? JSON.parse(rawValue) : rawValue;
         },
-        set(value) {
-          // Ensure value is stored as a JSON string
-          this.setDataValue("img_url", JSON.stringify(value));
-        },
       },
       title: {
         type: DataTypes.STRING,
--- src/models/research.js
+++ src/models/research.js
@@ -19,10 +19,6 @@ module.exports = (sequelize, DataTypes) => {
           // Parse JSON string if MariaDB stores it as a string
           return typeof rawValue === "string" ? JSON.parse(rawValue) : rawValue;
         },
-        set(value) {
-          // Ensure value is stored as a JSON string
-          this.setDataValue("media_url", JSON.stringify(value));
-        },
       },
       is_img: {
         type: DataTypes.BOOLEAN,
